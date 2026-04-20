4e5d4d8135f7a9d2bd0a8cd29209cb322667d5d8
Michael Helfenstein <michael.helfenstein@noser.com>
2026-04-20 19:59:31 +0200

    github action docker push in fork

--- scripts/ghcr-push.sh
+++ scripts/ghcr-push.sh
@@ -10,29 +10,20 @@ export GIT_VERSION=$(git describe --tags --abbrev=0)
 # GIT_COMMIT_SHORT - the short git commit number, like a718ef0.
 export GIT_COMMIT_SHORT=$(git rev-parse --short HEAD)
 # DOCKER_REPO - the base repository name to push the docker build to.
-export DOCKER_REPO=$DOCKER_USER/tile38
+export DOCKER_REPO=ghcr.io/$GITHUB_USER/tile38
 
 if [ "$GIT_BRANCH" != "master" ]; then
 	echo "Not pushing, not on master"
-elif [ "$DOCKER_USER" == "" ]; then
-	echo "Not pushing, DOCKER_USER not set"
-	exit 1
-elif [ "$DOCKER_LOGIN" == "" ]; then
-	echo "Not pushing, DOCKER_LOGIN not set"
-	exit 1
-elif [ "$DOCKER_PASSWORD" == "" ]; then
-	echo "Not pushing, DOCKER_PASSWORD not set"
-	exit 1
+elif [ "$GITHUB_USER" == "" ]; then
+	echo "Not pushing, GITHUB_USER not set"
 else
 	# setup cross platform builder
 	# https://github.com/tonistiigi/binfmt
 	docker run --privileged --rm tonistiigi/binfmt --install all
 	docker buildx create --name multiarch --platform linux/amd64,linux/amd64/v2,linux/amd64/v3,linux/arm64,linux/386,linux/arm/v7 --use default
 
-	# docker login
-	echo $DOCKER_PASSWORD | docker login -u $DOCKER_LOGIN --password-stdin
-	if [ "$(curl -s https://hub.docker.com/v2/repositories/$DOCKER_REPO/tags/$GIT_VERSION/ | grep "digest")" == "" ]; then
-		# build the docker image
+	if ! docker manifest inspect $DOCKER_REPO:$GIT_VERSION > /dev/null 2>&1; then
+  	# build the docker image
 		docker buildx build \
 			-f Dockerfile \
 			--platform linux/arm64,linux/amd64 \
